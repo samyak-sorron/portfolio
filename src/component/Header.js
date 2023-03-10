14dfcd71bfd6408124078517754d1113077ac9d6
samyak <samyak.sharma.758@gmail.com>
2023-03-10 10:59:15 +0530

    text in typing manner

---
+++ src/component/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div className='container self'>
+        <pre>
+          <span className='cyan'>&lt;b&gt;HELLO&lt;/b&gt;</span><br/>
+          
+        </pre>
+        <img src='#' alt='hello guys'></img>
+      </div>
+    </header>
+  )
+}
+
+export default Header
