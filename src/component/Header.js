27ae9871a2abafe326e8a556a7904093436c8420
samyak <samyak.sharma.758@gmail.com>
2023-03-12 17:07:38 +0530

    setting up the img at bottom

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,27 +1,35 @@
 import React from 'react'
 import Typewriter from 'typewriter-effect';
 import pfp from './Capture.PNG';
+import hero from './hero-devices.svg'
 
 const Header = () => {
   return (
     <header>
-      <div className='container self'>
-        <pre className=''>
-          <span className='cyan'>
-            &lt;b&gt;
-            <Typewriter options={{
-              loop:true,
-              delay:120,
-              autoStart:true,
-              strings:[' Hello',' samyak',' Hi'],
-              }}
-            />
-            &lt;/b&gt;
-          </span>
-          <br/>
-          MySelf Samyak kr Sharma       
-        </pre>
-        <img src={pfp} alt='hello guys'></img>
+      <div className='container'>
+        <div className='self'>
+          <div className='col-6 intro'>
+            <pre className=''>
+              <span className='cyan'>
+                &lt;b&gt;
+                <Typewriter options={{
+                  loop:true,
+                  delay:120,
+                  autoStart:true,
+                  strings:[' Hello',' samyak',' Hi'],
+                  }}
+                />
+                &lt;/b&gt;
+              </span>
+              <br/>
+              <h3>Myself Samyak kr Sharma</h3>      
+            </pre>
+          </div>
+          <div className='col-6 pfp'>
+            <img src={pfp} alt='profile'/>
+          </div>
+        </div>
+        <img src={hero} className='hero' alt='desktop'/>
       </div>
     </header>
   )
